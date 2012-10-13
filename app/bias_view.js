b19e61aee5b6a864bcbb23606806638fd7f9a007
J. Nathan Matias <natematias@gmail.com>
2012-10-13 15:42:46 -0400

    draft v1

--- app/bias_view.js
+++ app/bias_view.js
@@ -2,16 +2,41 @@ var BiasView = Backbone.View.extend({
      
   events: function() {
     return {
+      "click #check_bias_button":"check_bias_button"
     }
   },
 
   initialize: function(){
     _.bindAll(this, 'render');
-    var that = this;
+    this.account_url="http://hypatia.media.mit.edu/db/peoplebrowsr_dummy/_design/accounts/_view/account"
+    this.result_template = _.template($("#result_template").html())
+    this.no_result_template = _.template($("#no_result_template").html())
   },
 
-  render: function(column){
+  check_bias_button: function(e){
+    account_name = $("#twitter_account").val();
+    $("#notification").remove();
+    this.check_bias(account_name);
+  },
+
+  check_bias: function(account_name){
+    that = this;
+    account_name = account_name.replace("@","");
+    query_string = '?key="' + account_name + '"&limit=1'
+    jQuery.getJSON(that.account_url+query_string,function(data){
+      if(_.has(data, "rows") && data["rows"].length > 0){
+        row = data["rows"][0].value
+        $("#results").prepend(that.result_template({row:row}));
+        console.log(row.account)
+      }else{
+        //TODO: RESPOND TO ACCOUNT NOT FOUND
+        el = $(that.no_result_template());
+        $("#notifications").append(el);
+      }
+    });
+  },
+
+  render: function(){
     var that = this;
-    console.log(1);
   },
 });
