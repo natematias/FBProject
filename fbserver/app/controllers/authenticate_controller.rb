661e8080bf9739ac8ffac590f88b8f2fb8d6e855
J. Nathan Matias <natematias@gmail.com>
2013-01-19 23:08:35 -0500

    share results

--- fbserver/app/controllers/authenticate_controller.rb
+++ fbserver/app/controllers/authenticate_controller.rb
@@ -1,14 +1,14 @@
 class AuthenticateController < ApplicationController
   def index
-    client = TwitterClient.get_client
+    client = TwitterClient.get_auth_client
     session[:request_token] = client.request_token(:oauth_callback => "http://hypatia.media.mit.edu:4000/authenticate/success")
     redirect_to session[:request_token].authorize_url
   end
  
   def success
-    oauth_token = params[:oauth_token]
-    oauth_verifier = params[:oauth_verifier]
-    client = TwitterClient.get_client
+    session[:oauth_token] = params[:oauth_token]
+    session[:oauth_verifier] = params[:oauth_verifier]
+    client = TwitterClient.get_auth_client
 
     access_token = client.authorize(
       session[:request_token].token,
@@ -16,6 +16,8 @@ class AuthenticateController < ApplicationController
       :oauth_verifier => params[:oauth_verifier]
     )
     @authorized = client.authorized?
+    user_client = TwitterClient.get_api_client(session[:oauth_token], session[:request_token].secret)
+    @friends = Twitter.friends("natematias")
   end
 
 end
